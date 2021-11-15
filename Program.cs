6ca8bab87da75c6989de165b1bcb32880ab6786a
lostedzahfia <oyekunlefarouk2001@gmail.com>
2021-11-15 13:43:55 +0100

    create a csharp line of code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OWNER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
